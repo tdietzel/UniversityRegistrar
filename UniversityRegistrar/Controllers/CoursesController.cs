efaf7aa135d30590e06a90d671db3d190b31562e
mejia-dev <aaron.mejia8484@gmail.com>
2024-02-05 10:24:33 -0800

    Add Index for Courses
    Co-authored-by: Trent Dietzel <tdietzel@users.noreply.github.com>
    Co-authored-by: boboflofo <boboflofo@users.noreply.github.com>

---
+++ UniversityRegistrar/Controllers/CoursesController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using UniversityRegistrar.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace UniversityRegistrar.Controllers
+{
+  public class CoursesController: Controller
+  {
+    private readonly UniversityRegistrarContext _db;
+
+    public CoursesController(UniversityRegistrarContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Courses.ToList());
+    }
+  }
+}
