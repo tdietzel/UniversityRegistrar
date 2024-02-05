37ba8a308b72ef8668db32887c2af4472a03a315
mejia-dev <aaron.mejia8484@gmail.com>
2024-02-05 09:42:16 -0800

    Add Students Index view
    Co-authored-by: Trent Dietzel <tdietzel@users.noreply.github.com>
    Co-authored-by: boboflofo <boboflofo@users.noreply.github.com>

---
+++ UniversityRegistrar/Controllers/StudentsController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using UniversityRegistrar.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace UniversityRegistrar.Controllers
+{
+  public class StudentsController : Controller
+  {
+    private readonly UniversityRegistrarContext _db;
+
+    public StudentsController(UniversityRegistrarContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Students.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+  }
+}
